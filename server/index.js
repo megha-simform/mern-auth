a8aeefb11694481c1beae6d3e8708cbb5cd1c52e
Dhaval Thakkar <dhaval.t@simformsolutions.com>
2023-06-18 23:08:30 +0530

    feat: connect mongodb using mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
+const port = process.env.PORT;
+const mongoDbUri = process.env.MONGODB_URI ?? '';
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// Connects to the MongoDB database and starts the server at the specified port.
+try {
+  mongoose.connect(mongoDbUri).then((result) => {
+    app.listen(port, () => {
+      console.log(`Connected db and running app at port ${port}`);
+    });
+  });
+} catch (error) {
+  console.log(error);
+}
