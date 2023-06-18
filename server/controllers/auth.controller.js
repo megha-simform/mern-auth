db2f8d124dd5cc325d4874f8c3b455d4b5bddb45
Dhaval Thakkar <dhaval.t@simformsolutions.com>
2023-06-18 23:48:50 +0530

    feat: added login api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,12 @@
 import User from '../models/user.js';
+import AccessToken from '../models/accessToken.js';
+import RefreshToken from '../models/refreshToken.js';
 import { createHash } from '../utils/helper.js';
+
+const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
+const REFRESH_TOKEN_SECRET_KEY = process.env.REFRESH_TOKEN_SECRET_KEY;
+const refreshTokenExpiryTime = process.env.REFRESH_TOKEN_EXPIRY_TIME;
+const accessTokenExpiryTime = process.env.ACCESS_TOKEN_EXPIRY_TIME;
 
 /**
  * Handle the signup process.
@@ -9,8 +16,9 @@ import { createHash } from '../utils/helper.js';
  * @param {Function} next - The next middleware function.
  * @returns {Promise<void>} - A promise that resolves when the signup process is complete.
  */
-export default async function postSignup(req, res, next) {
+export async function postSignup(req, res, next) {
   try {
+    console.log('hi')
     // Extract data from the request body
     const { name, email, password, confirmPassword, gender } = req.body;
 
@@ -49,5 +57,71 @@ export default async function postSignup(req, res, next) {
     // Handle any unexpected errors
     console.log('Error in postSignup', error);
     return res.status(500).json({ error: 'INTERNAL SERVER ERROR' });
+  }
+}
+
+/**
+ * Handle the login process.
+ *
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>} - A promise that resolves when the login process is complete.
+ */
+export async function postLogin(req, res, next) {
+  try {
+    // Extract email and password from the request body
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Check if email or password is empty
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Email or Password field is empty.' });
+    }
+
+    // Find the user based on the email
+    const user = await User.findOne({ email });
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exist. Please signup to login.' });
+    } else {
+      // Check if access token and refresh token exist for the user
+      const accessTokenExists = await AccessToken.findOne({ userId: user._id });
+      const refreshTokenExists = await RefreshToken.findOne({ userId: user._id });
+
+      if (accessTokenExists && matchPassword(user.password ?? '', password)) {
+        // Return a success response if the user is already logged in
+        res.status(200).json({ message: 'User is already logged in.', accessToken: accessTokenExists });
+      } else if (refreshTokenExists && matchPassword(user.password ?? '', password)) {
+        // Return a message to generate a new access token using the refresh token
+        res.status(200).json({
+          message:
+            "User's refresh token exists. Please make a POST request to the refresh-token endpoint to create a new access token",
+        });
+      } else {
+        if (matchPassword(user.password ?? '', password)) {
+          // Generate new access token and refresh token
+          const accessToken = generateToken(user._id, ACCESS_TOKEN_SECRET_KEY, accessTokenExpiryTime);
+          const refreshToken = generateToken(user._id, REFRESH_TOKEN_SECRET_KEY, refreshTokenExpiryTime);
+
+          // Save the access token and refresh token in the database
+          await AccessToken.create({ accessToken: accessToken, userId: user._id });
+          await RefreshToken.create({ refreshToken: refreshToken, userId: user._id, valid: true });
+
+          // Return success response with access token and refresh token
+          return res
+            .status(200)
+            .json({ message: `${user.name} is logged in.`, accessToken: accessToken, refreshToken: refreshToken });
+        } else {
+          // Return error response for wrong password
+          res.status(400).json({ error: 'Wrong Password' });
+        }
+      }
+    }
+  } catch (error) {
+    // Handle any unexpected errors
+    console.log('Error in postLogin', error);
+    return res.status(500).json({ error: 'INTERNAL SERVER ERROR WHILE LOGGING IN.' });
   }
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
-import postSignup from '../controllers/auth.controller.js';
+import { postSignup, postLogin } from '../controllers/auth.controller.js';
 
 // Handle the POST request for the signup route.
 router.post('/signup', postSignup);
+
+// Handle the POST request for the login route.
+router.post('/login', postLogin);
 
 export default router;
