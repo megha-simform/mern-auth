3e3ff8cc15078e963c4fa0ab241e110b942486c3
megha-simform <megha.m@simformsolutions.com>
2023-11-26 08:37:00 +0530

    feat: add social api service & callback component

--- auth-react-app/src/apis/auth/index.tsx
+++ auth-react-app/src/apis/auth/index.tsx
@@ -1,1 +1,34 @@
 // add api methods
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000'; // Replace with your backend server URL
+
+const AuthAPIService = {
+  // Google OAuth
+  loginWithGoogle: () => {
+    window.location.href = `${API_BASE_URL}/auth/google`;
+  },
+
+  // Facebook OAuth
+  loginWithFacebook: () => {
+    window.location.href = `${API_BASE_URL}/auth/facebook`;
+  },
+
+  // Microsoft OAuth
+  loginWithMicrosoft: () => {
+    window.location.href = `${API_BASE_URL}/auth/microsoft`;
+  },
+
+  // Callback after OAuth authentication
+  handleAuthentication: () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    if (code) {
+      return axios.post(`${API_BASE_URL}/auth/callback`, { code })
+        .then(response => response.data);
+    }
+    return Promise.reject('Authentication failed');
+  },
+};
+
+export default AuthAPIService;
